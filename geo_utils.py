0604d22b1cb344cde82b4cb287298ebcc52100d1
mygadgetlife <steventgray@googlemail.com>
2026-08-21 16:09:52 +0100

    Change ZOOM level from 12 to 6

--- geo_utils.py
+++ geo_utils.py
@@ -14,7 +14,7 @@ reason about while plotting the UK, which is itself taller than it is wide.
 import math
 
 TILE_SIZE = 256
-ZOOM = 12
+ZOOM = 6
 
 # Logical (portrait) output size -- what you actually see once the panel
 # is physically mounted rotated 90 degrees.
